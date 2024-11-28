7bcb8c1b06e185dd9373c52fe592f18afe90e7ab
MangoEric <fgh1937@yahoo.com>
2024-11-29 00:24:44 +0900

    feat: search component init

--- static/hello-world/src/App.js
+++ static/hello-world/src/App.js
@@ -4,6 +4,7 @@ import { invoke } from '@forge/bridge';
 import ForceGraph3D from 'react-force-graph-3d';
 import SpriteText from 'three-spritetext';
 import randomColor from 'randomcolor';
+import Search from './components/search';
 
 function App() {
   const [graphData, setGraphData] = useState({ nodes: [], links: [] });
@@ -16,8 +17,32 @@ function App() {
     setGraphData(result)
   }, []);
 
+  const [searchWord, setSearchWord] = useState('');
+
+  const handleSearch = async () => {
+    const searchedPageIdList = await invoke('searchByAPI', { searchWord });
+    // console.log(searchedPageIdList, 'searchedPageIdList');
+    let oldNodes = graphData.nodes;
+    for(const node of oldNodes) {
+      if(searchedPageIdList.includes(node.id)) {
+        node.searched = true;
+      } else {
+        node.searched = false;
+      }
+    }
+    setGraphData({
+      nodes: oldNodes,
+      links: graphData.links,
+    })
+  };
+  
   return (
     <div>
+      <Search
+        searchWord={searchWord}
+        setSearchWord={setSearchWord}
+        handleSearch={handleSearch}
+      />
       <ForceGraph3D
           graphData={graphData}
           width={1500}
@@ -27,7 +52,7 @@ function App() {
           nodeThreeObject={node => {
             const sprite = new SpriteText(node.title);
             sprite.color = randomColor()
-            sprite.textHeight = 8;
+            node.searched ? sprite.textHeight = 30 : sprite.textHeight = 8;
             return sprite;
           }}
       />
