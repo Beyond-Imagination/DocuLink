6ac2cb62561c0e18fccf285c9299a9a078fc08c9
laggu91 <laggu91@gmail.com>
2024-12-02 00:22:31 +0900

    feat: swithcing 2d & 3d

--- static/hello-world/src/App.js
+++ static/hello-world/src/App.js
@@ -2,12 +2,15 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import { invoke, router } from '@forge/bridge';
 import ForceGraph3D from 'react-force-graph-3d';
+import ForceGraph2D from 'react-force-graph-2d';
 import SpriteText from 'three-spritetext';
 import randomColor from 'randomcolor';
 import Search from './components/search';
+import SwitchButton from "./components/SwitchButton";
 
 function App() {
   const [graphData, setGraphData] = useState({ nodes: [], links: [] });
+  const [is3D, setIs3D] = useState(false)
 
   useEffect(async () => {
     const result = await invoke('getGraphs');
@@ -55,24 +58,64 @@ function App() {
           handleSearch={handleSearch}
           handleSearchReset={handleSearchReset}
         />
-        <ForceGraph3D
-            graphData={graphData}
-            width={1500}
-            height={800}
-            linkOpacity={0.8}
-            controlType={'orbit'}
-            nodeThreeObject={node => {
-              const sprite = new SpriteText(node.title); 
-              node.searched ? sprite.textHeight = 30 : sprite.textHeight = 10;
-              node.searched ? sprite.color = '#ffde21' : sprite.color = '#ffffff';
-              return sprite;
-            }}
-            onNodeClick={(node) => {
-              if (node.url) {
-                router.open(node.url);
-              }
-            }}
+        <SwitchButton
+            is3D={is3D}
+            setIs3D={setIs3D}
         />
+          { is3D ?
+              <ForceGraph3D
+                  graphData={graphData}
+                  width={1500}
+                  height={800}
+                  linkOpacity={0.8}
+                  // backgroundColor={'black'}
+                  controlType={'orbit'}
+                  nodeThreeObject={node => {
+                      const sprite = new SpriteText(node.title);
+                      node.searched ? sprite.textHeight = 30 : sprite.textHeight = 10;
+                      node.searched ? sprite.color = '#ffde21' : sprite.color = '#ffffff';
+                      return sprite;
+                  }}
+                  onNodeClick={(node) => {
+                      if (node.url) {
+                          router.open(node.url);
+                      }
+                  }}
+              /> :
+              <ForceGraph2D
+                  graphData={graphData}
+                  width={1500}
+                  height={800}
+                  linkColor={() => 'rgba(255,255,255,0.8)'}
+                  backgroundColor={'rgba(0,0,16,255)'}
+                  nodeCanvasObject={(node, ctx, globalScale) => {
+                    const label = node.title;
+                    const fontSize = 12/globalScale;
+                    ctx.font = `${fontSize}px Sans-Serif`;
+                    const textWidth = ctx.measureText(label).width;
+                    const bckgDimensions = [textWidth, fontSize].map(n => 2*n); // some padding
+
+                    ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+
+                    ctx.textAlign = 'center';
+                    ctx.textBaseline = 'middle';
+                    ctx.fillStyle = node.color;
+                    ctx.fillText(label, node.x, node.y);
+
+                    node.__bckgDimensions = bckgDimensions; // to re-use in nodePointerAreaPaint
+                  }}
+                  nodePointerAreaPaint={(node, color, ctx) => {
+                    ctx.fillStyle = color;
+                    const bckgDimensions = node.__bckgDimensions;
+                    bckgDimensions && ctx.fillRect(node.x - bckgDimensions[0] / 2, node.y - bckgDimensions[1] / 2, ...bckgDimensions);
+                  }}
+                  onNodeClick={(node) => {
+                    if (node.url) {
+                      router.open(node.url);
+                    }
+                  }}
+              />
+          }
       </div>
     </div>
   );
---
+++ static/hello-world/src/components/SwitchButton.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const SwitchButton = ({is3D, setIs3D}) => {
+    const handleCheckboxChange = () => {
+        setIs3D(!is3D)
+    }
+
+    return (<>
+        <label className='themeSwitcherTwo relative inline-flex cursor-pointer select-none items-center'>
+            <input
+                type='checkbox'
+                checked={is3D}
+                onChange={handleCheckboxChange}
+                className='sr-only'
+            />
+            <span className='label flex items-center text-sm font-medium text-black'>
+              2D
+            </span>
+            <span
+                className={`slider mx-4 flex h-8 w-[60px] items-center rounded-full p-1 duration-200 ${is3D ? 'bg-[#212b36]' : 'bg-[#CCCCCE]'}`}
+            >
+                <span
+                    className={`dot h-6 w-6 rounded-full bg-white duration-200 ${is3D ? 'translate-x-[28px]' : ''}`}
+                ></span>
+            </span>
+            <span className='label flex items-center text-sm font-medium text-black'>
+              3D
+            </span>
+        </label>
+    </>)
+}
+
+export default SwitchButton
