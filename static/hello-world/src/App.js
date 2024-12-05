1939fcc40b653462c1b438bd9265752f63d8140c
MangoEric <fgh1937@yahoo.com>
2024-12-05 20:45:15 +0900

    update: searching ui

--- static/hello-world/src/App.js
+++ static/hello-world/src/App.js
@@ -63,9 +63,7 @@ function App() {
 
   // checkbox example event
   const handleSayHello = (checked) => {
-    if (checked) {
-      console.log('Hello, world!');
-    }
+    checked ? console.log('Hello, world!') : console.log('Goodbye, world!');
   };
 
   useEffect(() => {
@@ -102,7 +100,17 @@ function App() {
         </div>
           {isSearching && (
             <div className="absolute inset-0 z-20 flex items-center justify-center bg-black bg-opacity-50">
-              <div className="text-white text-xl">Searching...</div>
+              <div className="animate-pulse flex flex-col items-center space-y-4">
+                <div className="text-slate-600 text-xl font-medium">DocuLink</div>
+                <div className="flex-1 space-y-6 py-1">
+                  <div className="h-2 bg-slate-600 rounded w-24"></div>
+                  <div className="space-y-2">
+                    <div className="grid grid-cols-3 gap-4">
+                      <div className="h-2 bg-slate-600 rounded col-span-2"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           )}
           { is3D ?
