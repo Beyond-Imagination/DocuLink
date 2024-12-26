fe5b2d396b8ace53f8ae28df78201a629edb8a39
MangoE <104367020+MangoEric@users.noreply.github.com>
2024-12-26 22:05:34 +0900

    Revert "feat/link-color"
    This reverts commit 8c8ee1462d473e943672e82ab17855de671327b9.

--- static/hello-world/src/App.js
+++ static/hello-world/src/App.js
@@ -7,7 +7,6 @@ import SpriteText from 'three-spritetext';
 import SearchBar from './components/SearchBar';
 import SwitchButton from "./components/SwitchButton";
 import CheckBox from "./components/Checkbox";
-import { getNodoColor } from './utils/utils';
 
 function App() {
   const [appWidth, setAppWidth] = useState(window.innerWidth);
@@ -141,13 +140,11 @@ function App() {
                 title='keyword'
                 onChecked={handleCheckbox}
                 tooltip='Connect pages by keyword'
-                color={getNodoColor('keyword')}
               />
               <CheckBox
                 title='page hierarchy'
                 onChecked={handleCheckbox}
                 tooltip='Connect pages by page hierarchy'
-                color={getNodoColor('hierarchy')}
               />
             </div>
           </div>
@@ -196,7 +193,12 @@ function App() {
                   height={800}
                   linkOpacity={0.8}
                   linkColor={link=> {
-                    return getNodoColor(link.type)
+                    if(link.type === 'keyword') {
+                      return '#ffff00'
+                    } else if (link.type === 'hierarchy') {
+                      return '#00ffff'
+                    }
+                    return '#ffffff'
                   }}
                   // backgroundColor={'black'}
                   controlType={'orbit'}
@@ -229,7 +231,12 @@ function App() {
                   width={appWidth}
                   height={800}
                   linkColor={link=> {
-                    return getNodoColor(link.type)
+                    if(link.type === 'keyword') {
+                      return '#ffff00'
+                    } else if (link.type === 'hierarchy') {
+                      return '#00ffff'
+                    }
+                    return '#ffffff'
                   }}
                   backgroundColor={'rgba(0,0,16,255)'}
                   nodeCanvasObject={(node, ctx, globalScale) => {
--- static/hello-world/src/components/Checkbox.jsx
+++ static/hello-world/src/components/Checkbox.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-function CheckBox({ title, onChecked, tooltip, color }) {
+function CheckBox({ title, onChecked, tooltip }) {
   const [isChecked, setIsChecked] = useState(false);
 
   const handleChange = (e) => {
@@ -21,12 +21,7 @@ function CheckBox({ title, onChecked, tooltip, color }) {
           onChange={handleChange}
           className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-lg focus:ring-blue-500 focus:ring-2'
         />
-        <label htmlFor="default-checkbox" className={`px-2 text-sm font-medium capitalize ${isChecked ? 'text-yellow-300' : 'text-white'}`}>{title}</label>
-        <div className='w-4 h-4 rounded-full border border-white'
-          style={{
-            backgroundColor: color
-          }}
-        ></div>
+        <label htmlFor="default-checkbox" className={`ms-2 text-sm font-medium capitalize ${isChecked ? 'text-yellow-300' : 'text-white'}`}>{title}</label>
       </div>
       {tooltip && (
           <span className="absolute right-full top-1/2 transform -translate-y-1/2 mr-3 px-2 py-1 bg-gray-800 text-white text-xs rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap">
