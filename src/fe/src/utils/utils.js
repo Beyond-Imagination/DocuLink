a0000e47f649a4dbf5b6f3d0ccfc47176a9fe771
Kuwon Sebastian Na <laggu91@gmail.com>
2024-12-28 18:00:51 +0900

    feat: labels (#19)

--- src/fe/src/utils/utils.js
+++ src/fe/src/utils/utils.js
@@ -1,10 +1,13 @@
 function getLinkColor(type) {
   if(type === 'keyword') {
-    return '#ffff00'
+    return '#F77575'
   } else if (type === 'hierarchy') {
-    return '#00ffff'
+    return '#62A4FF'
+  } else if (type === 'labels') {
+    return '#FAEB40'
   }
-  return '#ffffff'
+  return '#42E45A'
+  // return '#C273FF'
 }
 
 export { getLinkColor }
