3d43b56827d3d7f201190a0db28b776ab67ac14e
정은영 <48662662+rlooo@users.noreply.github.com>
2025-01-11 16:04:32 +0900

    Feature/ rovo keyword extraction (#25)
    * feat: Add keyword extraction feature using Rovo
    
    * feat: Extracted keywords from all documents using Rovo
    
    * feat: Change storage to Forge Storage API
    
    * feat: change key docs to nodes
    
    * feat: rovo keyword extraction
    
    * chore: Remove unused dependency 'nlcst-to-string'

--- src/be/api.js
+++ src/be/api.js
@@ -52,7 +52,7 @@ export async function getKeywordGraphs() {
                 const keywords = []
                 if (file.data.keywords) {
                     for (const keyword of file.data.keywords) {
-                        const word = toString(keyword.matches[0].node)
+                        const word = toString(keyword.matches[0].node) 
                         const docIds = keywordMap.get(word)
                         if (docIds) {
                             keywordMap.set(word, [...docIds, d.id])
@@ -386,3 +386,60 @@ export async function getLabels() {
 
     return links
 }
+
+export const fetchPageInfos = async () => {
+    let pageInfo = undefined;
+    
+      const response = await api.asUser().requestConfluence(route`/wiki/api/v2/pages?body-format=atlas_doc_format`, {
+        headers: {
+          'Accept': 'application/json'
+        }
+      });
+
+      const pageInfos = []
+      if (response.ok) {
+        const responseJson = await response.json();
+        for (const d of responseJson.results) {
+            const content = d.body['atlas_doc_format'].value;
+            pageInfo = {
+              id: d.id,
+              title: d.title,
+              content: content            
+            }
+
+            pageInfos.push(pageInfo)
+        }
+      } else {
+        console.error(`fetchPageOrBlogInfo: Error: ${response.status} ${response.statusText}`);
+      }
+   
+    return pageInfos;
+  }
+
+export const getDocumentInfo = async (pageId) => {
+    try {
+        const response = await api
+            .asApp()
+            .requestConfluence(route`/wiki/rest/api/content/${pageId}`, {
+                headers: {
+                    Accept: 'application/json',
+                },
+            });
+
+        if (response.ok) {
+            const data = await response.json();
+            const title = data.title;
+            const url = data._links.base + data._links.webui;
+            const status = data.status;
+            const authorName = data.history.createdBy.username;
+            const createdAt = data.createdDate;
+            return { title, url, status, authorName, createdAt };
+        } else {
+            console.error(`Error in getDocumentInfo: ${response.status} ${response.statusText}`);
+            throw new Error(`Failed to fetch document info: ${pageId} ${response.status} ${response.statusText}`);
+        }
+    } catch (error) {
+        console.error(`Error in getDocumentInfo: ${error.message}`);
+        throw error;
+    }
+  };
